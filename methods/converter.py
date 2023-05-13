9c072ce8f4425b9014216302d894325a093c3999
Laszlo Lang <81311498+LzLang@users.noreply.github.com>
2023-05-13 15:55:36 +0200

    v0.0.5
    Cleaned up the script
    -> outsourced the methods/functions

--- methods/converter.py
+++ methods/converter.py
@@ -1,33 +1,6 @@
-import json
-import numpy as np
+import convertAPI as API
 
-# Open json file
-f = open('Study.json')
-# Open text document
-file = open('test-0.0.4.txt', 'a')
+input = API.readJSON("", "Study.json")
 
-# load Data from json
-data = json.load(f)
-# Only properties are needed
-properties = data['properties']
-
-# Allowed data types
-allowedTypes = ["boolean", "string"]
-
-# Every property
-for prop in properties:
-    # current property
-    property = properties[prop]
-    # Description
-    description = property['description']
-    # Type
-    type = property['type']
-    
-    # Checking if property has allowed data types
-    boolean_mask = np.nonzero(np.isin(allowedTypes, type))[0]    
-    if len(boolean_mask):
-        file.write(prop+"\n"+"description: "+description+"\n"+"type: "+str(type[1])+"\n\n")
-
-# close the files  
-file.close()
-f.close()
+propData = API.getPropData(input)
+API.writeJSON("", "test-0.0.5.json", propData)
