e830b0a9b2f9088dd448fde0f92189ae33ca1a18
Laszlo Lang <81311498+LzLang@users.noreply.github.com>
2023-05-13 15:39:46 +0200

    converter v0.0.2
    Update of version 0.0.1:
    - Extracting only the properties
    -> Input is saved as dictionary
    --> Using index "properties"

--- methods/converter.py
+++ methods/converter.py
@@ -3,15 +3,17 @@ import json
 # open a json file as input
 f = open('Study.json')
 # open a test file for output
-file = open('test-0.0.1.txt', 'a')
+file = open('test-0.0.2.txt', 'a')
 
 # read/load the data from the json file
 data = json.load(f)
+# extracting the properties
+properties = data['properties']
 
 # iterate through the data a write it to the output-file
-for i in data:
+for i in properties:
     file.write(i+"\n")
-    for j in data[i]:
+    for j in properties[i]:
         file.write(j+"\n")
     file.write("\n")
         
