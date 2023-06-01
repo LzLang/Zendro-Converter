17de91a9792b25adc7b9fcb3b28efca2ed4997e2
Laszlo Lang <81311498+LzLang@users.noreply.github.com>
2023-06-01 16:31:39 +0200

    converter.py v0.0.6
    Use startup arguments to a json file and a output path.
    Output file is named after input file and stored at the given path.
    Example:
    python convert.py "..\BrAPI_JSON_Schema_2-1\schemas\BrAPI-Core\Study.json" "..\output"

--- methods/converter.py
+++ methods/converter.py
@@ -1,6 +1,22 @@
-import convertAPI as API
+#   Imports
+import sys
+import convertAPI
+##############################
 
-input = API.readJSON("", "Study.json")
 
-propData = API.getPropData(input)
-API.writeJSON("", "test-0.0.5.json", propData)
+#   Main code
+# If not 3 arguments are given throw an error
+if len(sys.argv) != 3:
+    raise Exception("Use: python convert.py [input file] [output folder]")
+else:
+    # Load the content of the json file
+    input_file = convertAPI.read_json(sys.argv[1])
+    # Path, were the output file should be stored / placed
+    output_path = sys.argv[2]
+    # Get name of the input file and set it as filename for the output
+    output_filename = sys.argv[1][sys.argv[1].rfind("\\"):]
+
+    # Get properties with Zendro compatible datatype from the input file
+    properties = convertAPI.get_data(input_file)
+    # Write the properties to a json output file
+    convertAPI.write_json(output_path, output_filename, properties)
