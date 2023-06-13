6d3d8ac544cf6ecf47c790aec1b6103bfd0ed37a
Laszlo Lang <81311498+LzLang@users.noreply.github.com>
2023-06-13 17:13:27 +0200

    Automatic input and output

--- methods/convertAPI.py
+++ methods/convertAPI.py
@@ -1,4 +1,5 @@
 #   Imports
+import os
 import json
 import numpy as np
 from datetime import datetime, date
@@ -16,27 +17,97 @@ ZENDRO_TYPES = {
 
 
 #   Functions
-def log(msg):
-    """Writes the message to a log-file.\n
-    Logs the date and time automatically.\n
-    Date - Time: msg
+def setup_hierarchy(input_path, output_path):
+    """
+    Walks through the input path and searches for json files.\n
+    Also extracts the relative path to the json file for the output path.
+
+    :param input_path: Path to the input hierarchy
+    :param output_path: Path to the output hierarchy
+    :return: Returns all found files in the input hierarchy
     """
 
-    try:
-        # Get current date and time and write this with the message to the log file
-        with open("Log.txt", "a") as file:
-            current_time = datetime.now().strftime("%H:%M:%S")
-            file.write(str(date.today()) + " - " + current_time + ":\t" + msg + "\n")
-    except OSError as error:
-        # Prints the occurred error
-        print(error)
-        print("Not allowed to create or edit the log")
+    # All found files
+    input_files = []
+    # Relative paths to the files
+    output_hierarchy = []
+
+    # Walks through the input path
+    for root, dir, files in os.walk(input_path):
+        for filename in files:
+            # Only json files are needed
+            if '.json' in filename.lower():
+                # Append the file
+                input_files.append(os.path.join(root, filename))
+                # Current / relative path to the file fpr output hierarchy
+                current_path = root.replace(input_path+"\\", '')
+                if not current_path in output_hierarchy:
+                    output_hierarchy.append(current_path)
+
+    # Call function to create output hierarchy
+    create_output_hierarchy(output_path, output_hierarchy)
+
+    return input_files
+
+
+def create_output_hierarchy(output_path, hierarchy):
+    """
+    Creates the given hierarchy in the given path.
+
+    :param output_path: Path for the hierarchy output
+    :param hierarchy: Hierarchy that should be created
+    """
+    # For every directory in the hierarchy creates a directory
+    for directory in hierarchy:
+        path = os.path.join(output_path, directory)
+        try:
+            os.mkdir(path)
+        # Directory already exists, log this
+        except OSError as error:
+            log(error)
+
+
+def get_data(file_data):
+    """
+    From the passed data the properties are extracted.
+    :param file_data: Data from a json file
+    :return: Properties with a compatible type to Zendro
+    """
+
+    properties = file_data['properties']
+    properties_data = {}
+    for current_property in properties:
+        property_type = get_type(properties[current_property]['type'])
+        if property_type is None:
+            continue
+        if not 'description' in properties[current_property]:
+            continue
+        properties_data[current_property] = {
+            'description': properties[current_property]['description'],
+            'type': property_type
+        }
+    return properties_data
+
+
+def get_type(types):
+    """
+    Checks if the passed type is Zendro compatible.
+
+    :param types: Type of the property
+    :return: Compatible type or none if not compatible
+    """
+
+    boolean_mask = np.nonzero(np.isin(ALLOWED_TYPES, types))[0]
+    if len(boolean_mask):
+        return ZENDRO_TYPES[ALLOWED_TYPES[boolean_mask[0]]]
+    return None
 
 
 def read_json(file):
-    """Reads in a json file and returns its content.\n
-    Keyword arguments:\n
-    - file: Path to the file JSON that should be read
+    """
+    Reads in a json file and returns its content.
+
+    :param file: Path to the file JSON that should be read
     """
 
     try:
@@ -48,48 +119,36 @@ def read_json(file):
         log("Couldn't open file: " + file + "\tError: " + str(error))
 
 
-def write_json(path, filename, properties):
-    """Writes the passed data to a json file.\n
-    Keyword arguments:\n
-    - path: Path where the file should be saved\n
-    - filename: Filename\n
-    - property: Data that should be saved in json format
+def write_json(path, properties):
+    """
+    Writes the passed data to a json file.
+
+    :param path: Path where the file should be saved (incl. filename)
+    :param properties: Data that should be saved in json format
     """
 
     try:
         json_object = json.dumps(properties, indent=4)
-        with open(path + filename, "w") as file:
+        with open(path, "w") as file:
             file.write(json_object)
     except OSError as error:
-        log("Couldn't open file: " + path + filename + "\tError: " + str(error))
+        log("Couldn't open file: " + path + "\tError: " + str(error))
 
 
-def get_type(types):
-    """Checks if the passed type is Zendro compatible.\n
-    - If compatible: return the type.\n
-    - Else: return None.
+def log(msg):
+    """
+    Writes the message to a log-file.\n
+    Logs the date and time automatically.
+
+    :param msg: Message to log
     """
 
-    boolean_mask = np.nonzero(np.isin(ALLOWED_TYPES, types))[0]
-    if len(boolean_mask):
-        return ZENDRO_TYPES[ALLOWED_TYPES[boolean_mask[0]]]
-    return None
-
-
-def get_data(file_data):
-    """From the passed data the properties are extracted.\n
-    Properties with a type that is compatible to Zendro, will be stored in propData.\n
-    Returns propData, therefore only properties with compatible type.
-    """
-
-    properties = file_data['properties']
-    properties_data = {}
-    for current_property in properties:
-        property_type = get_type(properties[current_property]['type'])
-        if property_type is None:
-            continue
-        properties_data[current_property] = {
-            'description': properties[current_property]['description'],
-            'type': property_type
-        }
-    return properties_data
+    try:
+        # Get current date and time and write this with the message to the log file
+        with open("Log.txt", "a") as file:
+            current_time = datetime.now().strftime("%H:%M:%S")
+            file.write(str(date.today()) + " - " + current_time + ":\t" + msg + "\n")
+    except OSError as error:
+        # Prints the occurred error
+        print(error)
+        print("Not allowed to create or edit the log")
