dfcac0399ba8dd5ee3915c86362e891e881ff389
Laszlo Lang <81311498+LzLang@users.noreply.github.com>
2023-08-26 18:13:14 +0200

    Update to newest version

--- methods/convert.py
+++ methods/convert.py
@@ -1,8 +1,6 @@
 #   Imports
-import sys
 import convertAPI
 import argparse
-import os
 
 ##############################
 
@@ -23,7 +21,8 @@ parser.add_argument('-o', '--output-path',
 # Define a storage type
 parser.add_argument('-s', '--storage-type',
                     help='Type of storage where the model is stored.',
-                    choices=['sql', 'generic', 'zendro-server', 'cassandra', 'mongodb', 'neo4j', 'presto/trino', 'amazon-s3', 'distributed-data-model', 'adapter'],
+                    choices=['sql', 'generic', 'zendro-server', 'cassandra', 'mongodb', 'neo4j',
+                             'presto/trino', 'amazon-s3', 'distributed-data-model', 'adapter'],
                     default='sql')
 
 # Define a primary key
@@ -39,25 +38,17 @@ parser.add_argument('-t', '--primary-key-type',
 ##############################
 
 #   Main code
-# If not 3 arguments are given throw an error
 # Parse arguments
 args = parser.parse_args()
+
+# Search for json files and write them to a variable
 input_files = convertAPI.setup_hierarchy(args.input_path, args.output_path)
-for file in input_files:
-    # Load the content of the json file
-    json_file = convertAPI.read_json(file)
 
-    # Generate data for the output file
-    if args.primary_key_name:
-        output_file = {args.primary_key_name: args.primary_key_type}
-    else:
-        output_file = {os.path.splitext(os.path.basename(file))[0]+"_id": args.primary_key_type}
+# Extract from all json files the data
+file_data = convertAPI.read_json(input_files, args.storage_type, args.primary_key_name, args.primary_key_type)
 
-    for key in list(json_file):
-        item = convertAPI.get_type(json_file[key])
-        if item is not None:
-            output_file[key] = item
-    # Set Path for the output file
-    output_path = file.replace(args.input_path, args.output_path)
-    # Write the output file
-    convertAPI.write_json(output_path, output_file, args)
+# Get all Zendro compatible attributes and references
+items = convertAPI.get_items(file_data)
+
+# Write the Zendro data model definitions
+convertAPI.write_json(items, args.output_path, args.storage_type)
--- methods/convertAPI.py
+++ methods/convertAPI.py
@@ -1,18 +1,26 @@
 #   Imports
 import os
 import json
-import numpy as np
 from datetime import datetime, date
+
+
 ##############################
 
 
 #   Constants / Types used in Zendro
-ALLOWED_TYPES = ["string", "integer", "boolean"]
+BrAPI_TYPES = ["string", "integer", "boolean", "number"]
 ZENDRO_TYPES = {
     'string': 'String',
     'integer': 'Int',
-    'boolean': 'Boolean'
+    'boolean': 'Boolean',
+    'number': 'Float'
 }
+
+
+ZENDRO_STORAGE_TYPES = ['sql', 'generic', 'zendro-server', 'cassandra', 'mongodb', 'neo4j',
+                        'presto/trino', 'amazon-s3', 'distributed-data-model', 'adapter']
+
+
 ##############################
 
 
@@ -33,7 +41,7 @@ def setup_hierarchy(input_path, output_path):
     output_hierarchy = []
 
     # Walks through the input path
-    for root, dir, files in os.walk(input_path):
+    for root, directories, files in os.walk(input_path):
         for filename in files:
             # Only json files are needed
             if os.path.splitext(filename)[1].lower() == '.json':
@@ -41,7 +49,7 @@ def setup_hierarchy(input_path, output_path):
                 input_files.append(os.path.join(root, filename))
                 # Current / relative path to the file fpr output hierarchy
                 current_path = os.path.basename(root)
-                if not current_path in output_hierarchy:
+                if current_path not in output_hierarchy:
                     output_hierarchy.append(current_path)
 
     # Call function to create output hierarchy
@@ -61,65 +69,169 @@ def create_output_hierarchy(output_path, hierarchy):
     for directory in hierarchy:
         path = os.path.join(output_path, directory)
         try:
-            os.mkdir(path)
+            os.makedirs(path, exist_ok=True)
         # Directory already exists, log this
-        except OSError as error:
-            log(error)
+        except OSError as file_error:
+            log(f"Error creating directory {path}: {file_error}")
 
 
-def get_type(file_data):
+def get_items(file_data):
     """
-    Checks the passed data for a Zendro compatible type and returns it
-    :param file_data: Data from a json file (a dictionary)
+    Get a formatted dictionary of data models with Zendro compatible data types and references.\n
+    Ready to be writen in json format.
+
+    :param file_data: Content of multiple json files as dictionary
+    :return: Formatted dictionary of data models compatible to Zendro
+    """
+    # Formatted dictionary attributes and associations
+    items = {}
+    # Walk through all data models in file_data
+    for model in file_data:
+        # Create for every model a separate dictionary
+        items[model] = {}
+        # Every models needs a primary key, for the database
+        items[model]['primary_key'] = file_data[model]['primary_key']
+        # Walk through every property of a data model
+        for item_property in file_data[model]['properties']:
+            # Current property
+            item = file_data[model]['properties'][item_property]
+            # Get type of the current property
+            item_type = get_type(item)
+            # Check if property has a Zendro compatible data type
+            if item_type:
+                if 'attributes' not in items[model]:
+                    items[model]['attributes'] = {}
+                # Add property to the dictionary
+                items[model]['attributes'].update({item_property: item_type})
+
+            # Property may be an association, check if it is an association
+            reference = get_reference(item)
+            if reference:
+                if 'associations' not in items[model]:
+                    items[model]['associations'] = {}
+                # Transform association to a Zendro compatible association
+                target = file_data[reference['target']]
+                items[model]['associations'].update({
+                    item_property: {
+                        'type': reference['type'],
+                        'implementation': 'foreignkey',
+                        'reverseAssociation': reference['reverseAssociation'],
+                        'target': reference['target'],
+                        'targetKey': list(target['primary_key'].keys())[0],
+                        'keysIn': model,
+                        'targetStorageType': target['targetStorageType']
+                    }
+                })
+    return items if items else None
+
+
+def get_type(item):
+    """
+    Checks the passed item for a Zendro compatible type and returns it
+    :param item: An item from a json file (a dictionary)
     :return: Returns Zendro compatible type or none
     """
-
     # walk through the items of the dictionary
-    for key, value in file_data.items():
-        if key.lower() == 'type':
-            # if the properties has no compatible type it is not needed therefore None is returned
-            boolean_mask = np.nonzero(np.isin(ALLOWED_TYPES, value))[0]
-            if len(boolean_mask):
-                return ZENDRO_TYPES[ALLOWED_TYPES[boolean_mask[0]]]
-    return None
+    zendro_item_type = None
+    if 'type' in item:
+        # if the properties has no compatible type it is not needed therefore None is returned
+        if isinstance(item['type'], list):
+            for item_type in item['type']:
+                if item_type in BrAPI_TYPES:
+                    zendro_item_type = ZENDRO_TYPES[item_type]
+        else:
+            if item['type'] in BrAPI_TYPES:
+                zendro_item_type = ZENDRO_TYPES[item['type']]
+
+    return zendro_item_type
 
 
-def read_json(file):
+def get_reference(item):
+    """
+    Get all associations of a given property
+
+    :param item: Property of a model definition
+    :return: A dictionary of the items association
+    """
+
+    # Checks a given item if it's an association
+    # When it's a association extract all necessary information and return it, otherwise return none
+    reference = {}
+    if 'relationshipType' in item:
+        reference['type'] = item['relationshipType'].replace('-', '_')
+    if 'referencedAttribute' in item:
+        reference['reverseAssociation'] = item['referencedAttribute']
+    if '$ref' in item:
+        reference['target'] = item['$ref'].split('/')[-1]
+    elif 'items' in item and '$ref' in item['items']:
+        reference['target'] = item['items']['$ref'].split('/')[-1]
+    return reference if reference else None
+
+
+def read_json(files, storage_type, primary_key_name, primary_key_type):
     """
     Reads in a json file and returns its content.
 
-    :param file: Path to the file JSON that should be read
+    :param files: List of Json files to be read
+    :param storage_type: A Zendro compatible storage type (used database)
+    :param primary_key_name: Name of the primary key
+    :param primary_key_type: Type of the primary key (Int or String)
+    :return: Returns a dictionary of data model definitions
     """
-
     try:
+        # Raise an exception if storage_type or primary_key_type is not compatible
+        if storage_type not in ZENDRO_STORAGE_TYPES:
+            raise Exception('\'storage_type\' is not compatible to Zendro')
+        if primary_key_type not in ['Int', 'String']:
+            raise Exception('\'primary_key_type\' must be of type \'Int\' or \'String\'')
         # Open file and load json content
-        with open(file, "r") as json_file:
-            data = json.load(json_file)['properties']
-            return data
-    except OSError as error:
-        log("Couldn't open file: " + file + "\tError: " + str(error))
+        file_data = {}
+        for file in files:
+            with open(file, "r") as json_file:
+                # Get current model and save all model definitions to a dictionary
+                model = os.path.splitext(os.path.basename(file))[0]
+                file_data[model] = {}
+                file_data[model]['targetStorageType'] = storage_type
+                if primary_key_name:
+                    file_data[model]['primary_key'] = {primary_key_name: primary_key_type}
+                else:
+                    file_data[model]['primary_key'] = {f"{model}_id": primary_key_type}
+                file_data[model].update({'properties': json.load(json_file)['$defs'][model]['properties']})
+        return file_data if file_data else None
+    except OSError as file_error:
+        log(f"Couldn't open files: {file_error}")
 
 
-def write_json(path, properties, args):
+def write_json(file_data, output_path, storage_type):
     """
     Writes the passed data to a json file.
 
-    :param path: Path where the file should be saved (incl. filename)
-    :param properties: Data that should be saved in json format
+    :param file_data: A dictionary filled with model definitions
+    :param output_path: Path where the converted files should be saved
+    :param storage_type: A Zendro compatible storage type (used database)
     """
 
     try:
-        # Der Plan ist eine neue Variable zu machen, die dann wie auf Github die Sachen macht
-        model = {
-            'model': os.path.splitext(os.path.basename(path))[0],
-            'storageType': args.storage_type,
-            'attributes': properties
-        }
-        json_object = json.dumps(model, indent=4)
-        with open(path, "w") as file:
-            file.write(json_object)
-    except OSError as error:
-        log("Couldn't open file: " + path + "\tError: " + str(error))
+        # Raise an exception if storage_type is not compatible
+        if storage_type not in ZENDRO_STORAGE_TYPES:
+            raise Exception('\'storage_type\' is not compatible to Zendro')
+        # Format every model to write it to a json file as Zendro compatible data model definition
+        for model in file_data:
+            json_file = {
+                'model': model,
+                'storageType': storage_type,
+                'attributes': file_data[model]['attributes']
+            }
+            # If a model has an association it is needed to be included
+            if 'associations' in file_data[model]:
+                json_file['associations'] = file_data[model]['associations']
+            # Correct json format needed
+            json_object = json.dumps(json_file, indent=4)
+            # Write file
+            with open(os.path.join(output_path, f"{model}.json"), "w") as file:
+                file.write(json_object)
+    except OSError as file_error:
+        log(f"Couldn't write to file {output_path}: {file_error}")
 
 
 def log(msg):
@@ -135,7 +247,7 @@ def log(msg):
         with open("Log.txt", "a") as file:
             current_time = datetime.now().strftime("%H:%M:%S")
             file.write(f"{str(date.today())} - {current_time}:\t{msg}\n")
-    except OSError as error:
+            print(f"An error occurred, please view the log file for details!")
+    except OSError as log_error:
         # Prints the occurred error
-        print(error)
-        print("Not allowed to create or edit the log")
+        print(f"An error occurred while writing the log file: {log_error}")
